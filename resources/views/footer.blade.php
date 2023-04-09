ce76c3d0a1e9c4bcffeaa6ebb370adcd193855fb
Maneth Pathiarana <manith.sampath@towcha.com.au>
2023-04-09 17:10:45 +0530

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+  <div class="container text-center text-dark">
+    <p class="mb-0">&copy; 2023 {{ env('APP_NAME') }} . All rights reserved.</p>
+
+  </div>
+</footer>
---
+++ resources/views/topheader.blade.php
@@ -0,0 +1,32 @@
+<nav class="top-nav navbar navbar-expand-lg">
+    <div class="container-fluid text-light">
+        <div class="ms-3 me-3 col-8">
+            <div class="row">
+                <div class="col-4 dates">
+                    <i class="fas fa-clock text-white"></i>
+                    <span class="ms-2">Monday - Friday 9AM - 5PM</span>
+                </div>
+                <div class="col-6 dates">
+                    <i class="fas fa-location-dot"></i>
+                    <span class="ms-2">Offices Queensland HQ, Victoria, Brisbane City Office</span>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-4">
+            <div class="row">
+                <div class="col-5 dates">
+                    <span class="ms-2">Visit our social pages</span>
+                </div>
+                <div class="col-7 dates">
+                    <a href="https://www.facebook.com/australaccountants/"><i class="fab fa-facebook-f text-light"></i></a>
+                    <a href="https://www.instagram.com/austral_accountants/"><i class="fab fa-instagram text-light"></i></a>
+                    <a href="https://www.pinterest.com/australaccountants//"><i class="fab fa-pinterest text-light"></i></a>
+                    <a href="https://www.linkedin.com/company/austral-certified-accountants/about/"><i class="fab fa-linkedin-in text-light"></i></a>
+                    <a href="https://twitter.com/AUSTRALACCOUNTS"><i class="fab fa-twitter text-light"></i></a>
+                    <a href="https://www.youtube.com/@AustralAccountants"><i class="fab fa-youtube text-light"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
